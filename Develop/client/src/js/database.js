ba3d6a2587a46d9d888cd4ed197fa26918d418b7
Kais Al Balkhi <qaiscyberlife@gmail.com>
2022-07-25 21:35:43 -0400

    formated the webpack file, installed dependencies and edit the JS file

---
+++ Develop/client/src/js/database.js
@@ -0,0 +1,66 @@
+import { openDB } from 'idb';
+
+const initdb = async () =>
+// We are creating a new databased named 'jate' which will be using version 1 of the database.
+  openDB('jate', 1, {
+    // Adds our data base Schema if it has not been initialized.
+    upgrade(db) {
+      if (db.objectStoreNames.contains('jate')) {
+        console.log('jate database already exists');
+        return;
+      }
+      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
+      console.log('jate database created');
+    },
+  });
+
+// TODO: Add logic to a method that accepts some content and adds it to the database
+
+// Not sure of the syntax of the put method. Ask the Tutor. 
+export const putDb = async (content) => {
+
+  console.log('ADD to the database');
+
+  // Connects to the database (database, version)
+  const jateDb = await openDB('jate', 1);
+
+  // Creates a new transaction and specifies the database and data privileges.
+  const tx = jateDb.transaction('jate', 'readonly')
+
+  // Opens the desired object 
+  const store = tx.objectStore('jate');
+
+  // Gets all the data in the database
+  const request = store.putAll();
+
+  // Gets confirmation of the request
+  const result = await request;
+  console.log('🥳- Congrats!!! The data has been saved to the data base!', result)
+}
+
+
+
+
+
+// TODO: Add logic for a method that gets all the content from the database
+export const getDb = async () => {
+  console.log('GET from database');
+
+  // Connects to the database (database, version)
+  const jateDb = await openDB('jate', 1);
+
+  // Creates a new transaction and specifies the database and data privileges.
+  const tx = jateDb.transaction('jate', 'readonly')
+
+  // Opens the desired object 
+  const store = tx.objectStore('jate');
+
+  // Gets all the data in the database
+  const request = store.getAll();
+
+  // Gets confirmation of the request
+  const result = await request;
+  console.log('🥳- Congrats!!! The data has been saved to the data base!', result)
+}
+
+initdb();
---
+++ Develop/client/webpack.config.js
@@ -0,0 +1,85 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const WebpackPwaManifest = require('webpack-pwa-manifest');
+const path = require('path');
+const { InjectManifest } = require('workbox-webpack-plugin');
+
+
+module.exports = () => {
+  return {
+    mode: 'development',
+    entry: {
+      main: './src/js/index.js',
+      install: './src/js/install.js',
+      cards: './src/js/cards.js'
+    },
+
+    // TODO: Add the correct output
+    output: {
+      filename: '[name].bundle.js',
+      path: path.resolve(__dirname, 'dist'),
+    },
+
+    // TODO: Add and configure workbox plugins for a service worker and manifest file.
+    plugins: [
+      // Webpack Plugin to generate HTML and inject bundles.
+      new HtmlWebpackPlugin({
+        template: './index/html',
+        title: 'Text Editor',
+      }),
+
+
+      // TODO: Add the correct modules
+      // Injects pur service worker
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'src-sw.js',
+      }),
+
+
+      // Creates a manifest.json file.
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: 'Jate Text Editor',
+        short_name: 'Jate',
+        description: 'This is my first PWA app. It is a Text Editor!',
+        background_color: '22ca3',
+        theme_color: '#22ca3',
+        start_url: './',
+        publicPath: './',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          }
+        ]
+      }),
+    ],
+
+    // TODO: Add CSS loaders and babel to webpack.
+    module: {
+      // CSS Loaders
+      rules: [
+
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          // We use babel-loader to use ES6.
+          use: {
+            loader: "babel-loader",
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+            },
+          },
+        },
+      ],
+    }
+  };
+}; 
